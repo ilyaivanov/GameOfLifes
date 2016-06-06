b42e70b600a96d6f7013212af0461265bc423200
Ilya Ivanov <static.ila@gmail.com>
2016-06-06 15:42:57 +0300

    Simple data structure to represent a board

---
+++ app/vanilla/game.manager.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    setBoard();
+    setInterval(setBoard, 1000);
+});
+
+
+function setBoard() {
+    var board = [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,1,0,0,0,0,0],
+        [0,0,0,0,1,1,0,0,0,0],
+        [0,0,0,0,1,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ];
+    
+    var box = document.getElementById("box");
+    clearNode(box);
+    for (var i = 0; i < 10; i++) {
+        var row = createRow(board[i]);
+        box.appendChild(row);
+    }
+
+    function createRow(row) {
+        var rowElement = document.createElement('div');
+        for (var i = 0; i < 10; i++) {
+            var newCell = document.createElement('div');
+            var className = "cell";
+            if (row[i])
+            {
+                className += " full";
+            }
+            newCell.className = className;
+
+            rowElement.appendChild(newCell);
+        }
+        return rowElement;
+    }
+
+    function clearNode(node){
+        while (node.firstChild) {
+            node.removeChild(node.firstChild);
+        }
+    }
+}
+
